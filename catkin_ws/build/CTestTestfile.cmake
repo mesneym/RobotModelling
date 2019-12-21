2d1631e9e3ddb13c6eb29d29bff934f5ae374ace
mesneym <mesney.mesney@gmail.com>
2019-12-21 07:14:38 -0500

    Removed unused folders

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ak/RobotModelling/catkin_ws/src
-# Build directory: /home/ak/RobotModelling/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("RobotArmUrdfMeshesProp")
--- catkin_ws/build/RobotArmUrdfMeshesProp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ak/RobotModelling/catkin_ws/src/RobotArmUrdfMeshesProp
-# Build directory: /home/ak/RobotModelling/catkin_ws/build/RobotArmUrdfMeshesProp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
